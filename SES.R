4b5c4e5d50699de2bb68b824b7865d34d791b2e5
O'Neal <ho185023@ncr.com>
2022-06-07 15:00:41 -0400

    Filtering for SES

--- SES.R
+++ SES.R
@@ -1,3 +1,7 @@
 #Simple Exponential Smoothing (SES)
 data <- read.csv("Data/supermarket_sales - Sheet1.csv")
 
+data.ts <- ts(data[ ,-1], frequency = 12, start = c(2019, 1, 1))
+head(data.ts)
+
+plot(data.ts[ ,9], data.ts[ ,8])
