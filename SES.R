d74f751e4f15585524174037b5ca3af86207127f
O'Neal <ho185023@ncr.com>
2022-06-07 15:03:43 -0400

    Attempt 2

--- SES.R
+++ SES.R
@@ -1,7 +1,37 @@
 #Simple Exponential Smoothing (SES)
+library(dplyr)
+
 data <- read.csv("Data/supermarket_sales - Sheet1.csv")
 
-data.ts <- ts(data[ ,-1], frequency = 12, start = c(2019, 1, 1))
-head(data.ts)
 
-plot(data.ts[ ,9], data.ts[ ,8])
+#data <- data %>%
+ # select("Date", "Total")
+#data <- data[order(as.Date(data$Date, format="%m/%d/%Y")),]
+#print(data)
+
+
+#data.ts <- ts(data[ ,-1], start = c(2019, 1, 1))
+
+
+#data.filter <- data[order(as.Date(data[ ,11], format="%m/%d/%Y")),]
+data.filter <- filter(data, data[ ,2] == 'A')
+data.filter <- filter(data.filter, data.filter[ ,4] == 'Member')
+data.filter <- filter(data.filter, data.filter[ ,6] == 'Health and beauty')
+
+
+data.filter.ts <- ts(data.filter[ ,-1], frequency = 12, start = c(1, 1, 2019))#, end = c(3, 31, 2019))
+
+data.filter.ts <- data.filter.ts[order(data.filter.ts[ ,10]), ]
+
+head(data.filter.ts)
+
+plot(x = data.filter.ts[ ,10], y = data.filter.ts[ ,9])
+lines(data.filter.ts)
+
+#head(data.ts)
+
+#plot(x = data.ts[ ,10], y = data.ts[ ,9])
+
+df[order(-df$service_price),]
+
+     
